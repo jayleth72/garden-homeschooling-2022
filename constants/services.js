bfa36ed2b4df5df00be14aa80f49e274cdf2f0c7
jayleth72 <jay.letheby@gmail.com>
2022-01-28 08:54:09 +1000

    added css module styles

---
+++ constants/services.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { FaHandHoldingHeart } from "react-icons/fa";
+import { GiAnatomy, GiThink } from "react-icons/gi";
+
+export default [
+  {
+    icon: <GiAnatomy />,
+    title: "Body",
+    text: "We move your body"
+  },
+  {
+    icon: <GiThink />,
+    title: "Mind",
+    text: "We activate your mind"
+  },
+  {
+    icon: <FaHandHoldingHeart />,
+    title: "Hand",
+    text: "We are hands on and hands in"
+  }
+];
